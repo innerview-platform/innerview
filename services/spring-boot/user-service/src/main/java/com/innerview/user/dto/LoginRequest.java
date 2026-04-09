2df8bde6bae9b9feb8dc9c103c94646fe24dd6d6
MostafaAbdulazziz <es-moustafa.abdelazziz2027@alexu.edu.eg>
2026-04-09 20:14:52 +0200

    INNER-19 Login-Logout-Testing
    Implemented UserService Unit Tests

--- services/spring-boot/user-service/src/main/java/com/innerview/user/dto/LoginRequest.java
+++ services/spring-boot/user-service/src/main/java/com/innerview/user/dto/LoginRequest.java
@@ -2,9 +2,11 @@ package com.innerview.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginRequest {
 
     @NotBlank(message = "Email can't be empty")
