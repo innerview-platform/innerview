c80f09d45d962fea53d1ce3e2e4e4c5051bdf343
MostafaAbdulazziz <es-moustafa.abdelazziz2027@alexu.edu.eg>
2026-04-19 15:43:20 +0200

    INNER-105 Interview Controller

---
+++ services/spring-boot/src/main/java/com/innerview/spring/dto/InterviewResponse.java
@@ -0,0 +1,10 @@
+package com.innerview.spring.dto;
+
+import com.innerview.spring.enums.InterviewStatus;
+
+public class InterviewResponse {
+	Long interviewId;
+	String roomId;
+	String roomLink;
+	InterviewStatus interviewStatus;
+}
